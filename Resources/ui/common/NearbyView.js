37f9633feb8e0eca6bd1876d65fa5413b76cbab6
Bilal Quadri <bilalquadri92@gmail.com>
2012-07-18 22:31:01 -0400

    Refactored code for nearby

--- Resources/ui/common/NearbyView.js
+++ Resources/ui/common/NearbyView.js
@@ -1,9 +1,18 @@
 module.exports = function(){
-  var self = Ti.UI.createScrollView({
+  var self = Ti.UI.createView({
+    width: Ti.UI.FILL,
+    height: Ti.UI.FILL,
+    backgroundImage: 'images/bg-noise-pattern.png',
+    backgroundRepeat: true
+  });
+    
+  var scrollView = Ti.UI.createScrollView({
     layout : 'vertical',
     contentHeight : 'auto',
     scrollType : 'vertical',
   });
+  self.add(scrollView);
+
 
   return self;
 };
