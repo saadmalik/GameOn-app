c613017b29e314d90f5b4c913601241d4482d6aa
Bilal Quadri <bilalquadri92@gmail.com>
2012-07-18 23:05:48 -0400

    Nearby view row icons added

--- Resources/ui/common/NearbyView.js
+++ Resources/ui/common/NearbyView.js
@@ -6,13 +6,43 @@ module.exports = function(){
     backgroundRepeat: true
   });
     
+  var line = Ti.UI.createView({
+    height: Ti.UI.FILL,
+    width: '5',
+    backgroundColor: '#ccc',
+    opacity: 0.1,
+    left: '9%',
+  });
+  self.add(line);
+
   var scrollView = Ti.UI.createScrollView({
     layout : 'vertical',
     contentHeight : 'auto',
     scrollType : 'vertical',
+    opacity: 1,
+    zIndex: 99
   });
   self.add(scrollView);
 
+  for (var i = 0; i < 20; i++) {
+    var row = Ti.UI.createView({
+      top: 15,
+      height: 100,
+      layout: 'horizontal',
+      zIndex: 99
+    });
+
+    var sportIcon = Ti.UI.createImageView({
+      image: 'images/soccer-icon.png',
+      top: 40,
+      left: '1%',
+      height: 29,
+      zIndex: 99
+    });
+    row.add(sportIcon);
+
+    scrollView.add(row);
+  }
 
   return self;
 };
