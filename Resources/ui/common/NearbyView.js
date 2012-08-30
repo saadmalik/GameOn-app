33869e96a6422283c2a0a2ee366ba4e3e975b177
Bilal Quadri <bilalquadri92@gmail.com>
2012-08-30 16:54:54 -0400

    Mock data now being used for nearby view

--- Resources/ui/common/NearbyView.js
+++ Resources/ui/common/NearbyView.js
@@ -17,7 +17,7 @@ var gamesList = [{
 module.exports = function(){
   var basePath = Ti.Platform.osname === 'android' ? 'ui/android/' : 'ui/iphone/';
   var GameDetailWindow = require(basePath + 'GameDetailWindow');
-  
+
   var self = Ti.UI.createView({
     width: Ti.UI.FILL,
     height: Ti.UI.FILL,
@@ -44,6 +44,7 @@ module.exports = function(){
   self.add(scrollView);
 
   for (var i = 0; i < 20; i++) {
+    var gameObject = gamesList[0]; //Temporary; needs to be more modularly laid out later
     var row = Ti.UI.createView({
       top: 10,
       height: 100,
@@ -52,7 +53,7 @@ module.exports = function(){
     });
 
     var sportIcon = Ti.UI.createImageView({
-      image: 'images/soccer-icon.png',
+      image: 'images/'+gameObject.sport+'-icon.png',
       top: 37,
       left: '1%',
       height: 29,
@@ -68,120 +69,121 @@ module.exports = function(){
     row.add(gameInfoBox);	
     
     gameInfoBox.addEventListener('click',function() {
+      //MOVE THIS FUNCTION OUTSIDE OF LOOP LATER
       Titanium.API.info('Button Clicked');
       
     });
     
     var dateTimeCommentsBg = Ti.UI.createView({
-    	backgroundColor: '#f9f5f2',
-    	height:30,
-    	width:235,
-    	zIndex:999,
-    	right:2,
-    	bottom:3
+      backgroundColor: '#f9f5f2',
+      height:30,
+      width:235,
+      zIndex:999,
+      right:2,
+      bottom:3
     });
     gameInfoBox.add(dateTimeCommentsBg);
     
     var clockIcon = Ti.UI.createImageView({
-    	image: 'images/clock.png',
-    	width:11,
-    	bottom:'6%',
-    	left:'8%',
-    	zIndex:999
+      image: 'images/clock.png',
+      width:11,
+      bottom:'6%',
+      left:'8%',
+      zIndex:999
     });
     gameInfoBox.add(clockIcon);
     
     var dateTime = Ti.UI.createLabel({
-    	text: 'Today @ 8pm',
-    	color: '#b4b1b1', 
-    	font: { fontSize:13,fontFamily:'Helvetica Neue' },
-    	bottom: '8%',
-    	left:'15%',
-    	zIndex:999
+      text: 'Today @ 8pm',
+      color: '#b4b1b1', 
+      font: { fontSize:13,fontFamily:'Helvetica Neue' },
+      bottom: '8%',
+      left:'15%',
+      zIndex:999
     });
     gameInfoBox.add(dateTime);
     
     var commentBubble = Ti.UI.createImageView({
-    	image: 'images/commentBubble.png',
-    	right: '3%',
-    	bottom:'6%',
-    	height:20,
-    	width:22,
-    	zIndex:999
+      image: 'images/commentBubble.png',
+      right: '3%',
+      bottom:'6%',
+      height:20,
+      width:22,
+      zIndex:999
     });
     gameInfoBox.add(commentBubble);
     
     var numComments = Ti.UI.createLabel({
-    	text: '3',
-    	font: { fontSize:11,fontFamily:'Helvetica Neue' },
-    	color: '#b4b1b1',
-    	textAlign:'center',
-    	bottom:'20%',
-    	zIndex:999
+      text: ''+gameObject.comment_count,
+      font: { fontSize:11,fontFamily:'Helvetica Neue' },
+      color: '#b4b1b1',
+      textAlign:'center',
+      bottom:'20%',
+      zIndex:999
     });
     commentBubble.add(numComments);
     
     var profilePhoto = Ti.UI.createImageView({
-    	image: 'images/profile-photo.png',
-    	width: 49,
-    	height: 50,
-    	left:'7%',
-    	top:'7%',
-    	zIndex:999
+      image: gameObject.nearby_user.photo,
+      width: 49,
+      height: 50,
+      left:'7%',
+      top:'7%',
+      zIndex:999
     });
     gameInfoBox.add(profilePhoto);
     
     var userFullName = Ti.UI.createLabel({
-    	text: 'Saad Malik',
-    	left:'30%',
-    	top:'3%',
-    	font: { fontSize:13,fontFamily:'Helvetica Neue',fontWeight:'bold' },
-    	color: '#666'
+      text: gameObject.nearby_user.name,
+      left:'30%',
+      top:'3%',
+      font: { fontSize:13,fontFamily:'Helvetica Neue',fontWeight:'bold' },
+      color: '#666'
     });
     gameInfoBox.add(userFullName);
     
     var gameJoined = Ti.UI.createLabel({
-    	text: 'joined a game of Soccer',
-    	left:'30%',
-    	top:'19%',
-    	font: { fontSize:13,fontFamily:'Helvetica Neue' },
-    	color: '#959595'
+      text: 'joined a game of Soccer',
+      left:'30%',
+      top:'19%',
+      font: { fontSize:13,fontFamily:'Helvetica Neue' },
+      color: '#959595'
     });
     gameInfoBox.add(gameJoined);
     
     var geoPinIcon = Ti.UI.createImageView({
-    	image: 'images/geoPin.png',
-    	width:7,
-    	height:11,
-    	left:'30%',
-    	top:'43%'
+      image: 'images/geoPin.png',
+      width:7,
+      height:11,
+      left:'30%',
+      top:'43%'
     });
     gameInfoBox.add(geoPinIcon);
     
     var distance = Ti.UI.createLabel({
-    	text: '0.3 mi',
-    	font: { fontSize:13,fontFamily:'Helvetica Neue' },
-    	color: '#959595',
-    	top:'39%',
-    	left:'34%'
+      text: gameObject.distance + ' mi',
+      font: { fontSize:13,fontFamily:'Helvetica Neue' },
+      color: '#959595',
+      top:'39%',
+      left:'34%'
     });
     gameInfoBox.add(distance);
     
     var userIcon = Ti.UI.createImageView({
-    	image: 'images/user.png',
-    	width:12,
-    	height:11,
-    	left:'53%',
-    	top:'43%'
+      image: 'images/user.png',
+      width:12,
+      height:11,
+      left:'53%',
+      top:'43%'
     });
     gameInfoBox.add(userIcon);
     
     var capacity = Ti.UI.createLabel({
-    	text: '11/15',
-    	font: { fontSize:13,fontFamily:'Helvetica Neue' },
-    	color: '#959595',
-    	top:'39%',
-    	left: '59%'
+      text: gameObject.capacity,
+      font: { fontSize:13,fontFamily:'Helvetica Neue' },
+      color: '#959595',
+      top:'39%',
+      left: '59%'
     });
     gameInfoBox.add(capacity);
 
