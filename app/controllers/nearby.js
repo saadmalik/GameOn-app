283a6a451efd586f1beff4c0a395dc43b67c9d4a
Bilal Quadri <bilalquadri92@gmail.com>
2012-11-02 17:33:24 -0400

    Started creating instances of controllers

--- app/controllers/nearby.js
+++ app/controllers/nearby.js
@@ -1,7 +1,12 @@
+/* Add create button for iOS */
 if (OS_IOS) {
   $.nearbyWindow.rightNavButton = Ti.UI.createButton({
     title: 'Create'
   });
 }
 
-
+/* Add games to the scroll view */
+for (var i = 0; i < 10; i += 1) {
+  var gameBox = Alloy.createController('game_box').getView();
+  $.scroller.add(gameBox);
+}
