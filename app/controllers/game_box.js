efa788967eb909c0fe17e5bf74a6cfac29e3d9e0
Bilal Quadri <bilalquadri92@gmail.com>
2012-11-03 01:47:54 -0400

    Dynamically creating boxes based on data model

--- app/controllers/game_box.js
+++ app/controllers/game_box.js
@@ -1,6 +1,24 @@
 var args = arguments[0] || {};
 
+$.sport_icon.image = '/images/' + args.sport + '-icon.png';
+$.comment_count.text = args.comment_count;
+$.date_time.text = args.nearby_user.timestamp;
+  //{
+    //id: '12345',
+    //sport: "soccer",
+    //distance: '0.2',
+    //comment_count: 4,
+    //nearby_user: {
+      //id: '007',
+      //name: 'Saad Malik',
+      //photo: '/images/profile-photo.png',
+      //timestamp: '101321312'
+    //}
+  //},
+
 
 function gameClick(e) {
-  alert("Game: " + (args.sport || "none"));
+  alert(args.id);
 }
+
+//exports.id = args.id;
--- app/controllers/nearby.js
+++ app/controllers/nearby.js
@@ -6,20 +6,70 @@ if (OS_IOS) {
 }
 
 var games = [
-  "soccer",
-  "basketball",
-  "football",
-  "frisbee",
-  "volleyball",
-  "soccer",
-  "basketball",
-  "football",
-  "frisbee",
-  "volleyball"
+  {
+    id: '12345',
+    sport: "soccer",
+    distance: '0.2',
+    comment_count: 4,
+    nearby_user: {
+      id: '007',
+      name: 'Saad Malik',
+      photo: '/images/profile-photo.png',
+      timestamp: '101321312'
+    }
+  },
+  {
+    id: '12345',
+    sport: "soccer",
+    distance: '0.2',
+    comment_count: 4,
+    nearby_user: {
+      id: '007',
+      name: 'Saad Malik',
+      photo: '/images/profile-photo.png',
+      timestamp: '101321312'
+    }
+  },
+  {
+    id: '12345',
+    sport: "soccer",
+    distance: '0.2',
+    comment_count: 4,
+    nearby_user: {
+      id: '007',
+      name: 'Saad Malik',
+      photo: '/images/profile-photo.png',
+      timestamp: '101321312'
+    }
+  },
+  {
+    id: '12345',
+    sport: "soccer",
+    distance: '0.2',
+    comment_count: 4,
+    nearby_user: {
+      id: '007',
+      name: 'Saad Malik',
+      photo: '/images/profile-photo.png',
+      timestamp: '101321312'
+    }
+  },
+  {
+    id: '12345',
+    sport: "soccer",
+    distance: '0.2',
+    comment_count: 4,
+    nearby_user: {
+      id: '007',
+      name: 'Saad Malik',
+      photo: '/images/profile-photo.png',
+      timestamp: '101321312'
+    }
+  },
 ];
 
 /* Add games to the scroll view */
-for (var i = 0; i < 9; i += 1) {
-  var gameBox = Alloy.createController('game_box', {sport: games[i]}).getView();
+for (var i = 0; i < games.length; i += 1) {
+  var gameBox = Alloy.createController('game_box', games[i]).getView();
   $.scroller.add(gameBox);
 }
