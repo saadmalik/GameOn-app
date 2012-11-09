1e9c7e03c41b165857a57e2efb1cd485483d3b01
Bilal Quadri <bilalquadri92@gmail.com>
2012-11-08 21:13:40 -0500

    Game box views completely done

--- app/controllers/game_box.js
+++ app/controllers/game_box.js
@@ -1,9 +1,10 @@
 var datetime = require('alloy/datetime');
-
 var args = arguments[0] || {};
 
 
-/* Necessary calculations */
+
+
+// Necessary calculations
 var timestamp = new Date(args.nearby_user.timestamp);
 var date = datetime.formatAsShort(timestamp);
 if (date.indexOf('/') < 0) {
@@ -13,13 +14,19 @@ var sport = args.sport;
 sport = sport.charAt(0).toUpperCase() + sport.slice(1);
 
 
-/* Set values for game specific attributes */
+
+
+// Set values for game specific attributes
 $.sport_icon.image = '/images/' + args.sport + '-icon.png';
 $.comment_count.text = args.comment_count;
 $.date_time.text = date;
 $.profile_photo.image = args.nearby_user.photo;
 $.name.text = args.nearby_user.name;
 $.game_type.text = sport;
+$.distance.text = args.distance;
+$.headcount.text = args.spots;
+
+
 
 
 /* Action to take when a game is clicked */
--- app/controllers/nearby.js
+++ app/controllers/nearby.js
@@ -7,23 +7,23 @@ if (OS_IOS) {
 
 /* Sample Games Response */
 var games = [
-  {id: '12345', sport: "soccer", distance: '0.2', comment_count: 5,
+  {id: '12345', sport: "soccer", spots: '10/15', distance: '0.1', comment_count: 5,
     nearby_user: {id: '007', name: 'Saad Malik',
     photo: '/images/profile-photo.png', timestamp: 1351923356284 }
   },
-  {id: '12345', sport: "soccer", distance: '0.2', comment_count: 1,
+  {id: '12345', sport: "soccer", spots: '11/12', distance: '0.2', comment_count: 1,
     nearby_user: {id: '007', name: 'Saad Malik',
     photo: '/images/profile-photo.png', timestamp: 1351923530557 }
   },
-  {id: '12345', sport: "soccer", distance: '0.2', comment_count: 0,
+  {id: '12345', sport: "soccer", spots: '7/10', distance: '0.8', comment_count: 0,
     nearby_user: {id: '007', name: 'Saad Malik',
     photo: '/images/profile-photo.png', timestamp: 1351923356284 }
   },
-  {id: '12345', sport: "soccer", distance: '0.2', comment_count: 7,
+  {id: '12345', sport: "soccer", spots: '15/16', distance: '1.2', comment_count: 7,
     nearby_user: {id: '007', name: 'Saad Malik',
     photo: '/images/profile-photo.png', timestamp: 1351623356284 }
   },
-  {id: '12345', sport: "soccer", distance: '0.2', comment_count: 2,
+  {id: '12345', sport: "soccer", spots: '3/10', distance: '1.9', comment_count: 2,
     nearby_user: {id: '007', name: 'Saad Malik',
     photo: '/images/profile-photo.png', timestamp: 1350623356284 }
   }
