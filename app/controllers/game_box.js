54e5ccc49635a0ffd4699c2e35dca26ec290d781
Bilal Quadri <bilalquadri92@gmail.com>
2012-11-25 12:12:32 -0500

    Fixed widths and stuff

--- app/controllers/game_box.js
+++ app/controllers/game_box.js
@@ -15,7 +15,7 @@ sport = sport.charAt(0).toUpperCase() + sport.slice(1);
 
 
 // Set values for game specific attributes
-$.sport_icon.image = '/images/sport_icons' + args.sport + '.png';
+$.sport_icon.image = '/images/sport_icons/'+args.sport+'.png';
 $.comment_count.text = args.comment_count;
 $.date_time.text = date;
 $.profile_photo.image = args.nearby_user.photo;
