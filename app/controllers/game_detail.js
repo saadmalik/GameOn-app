8641ba0cd52305cc16ce8ae7a0ed094bbff83f84
Saad Malik <saadfmalik@gmail.com>
2012-12-02 17:39:04 -0500

    Fixed share button visibilty in game detail window

--- app/controllers/game_detail.js
+++ app/controllers/game_detail.js
@@ -3,7 +3,7 @@ var args = arguments[0] || {};
 Ti.API.info("ARGUMENTS PASSED TO GAME DETAIL: " + JSON.stringify(args));
 
 $.gameDetail.rightNavButton = Ti.UI.createButton({
-	backgroundImage: 'images/gameDetail/share.png',
+	backgroundImage: '/images/game_detail/share.png',
 	width: '43dp',
 	height: '29dp'
 });
