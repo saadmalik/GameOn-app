cc66c00b80d433d8717a965805aa78fcf0681f4c
Saad Malik <saadfmalik@gmail.com>
2012-12-02 17:22:23 -0500

    Added share button in game detail window

--- app/controllers/game_detail.js
+++ app/controllers/game_detail.js
@@ -2,3 +2,8 @@ var args = arguments[0] || {};
 
 Ti.API.info("ARGUMENTS PASSED TO GAME DETAIL: " + JSON.stringify(args));
 
+$.gameDetail.rightNavButton = Ti.UI.createButton({
+	backgroundImage: 'images/gameDetail/share.png',
+	width: '43dp',
+	height: '29dp'
+});
