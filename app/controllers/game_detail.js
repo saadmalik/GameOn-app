ed480672193692fee9f7c487ef37e9f33d41df26
Bilal Quadri <bilalquadri92@gmail.com>
2012-12-02 21:07:07 -0500

    Fixed problems

--- app/controllers/game_detail.js
+++ app/controllers/game_detail.js
@@ -2,8 +2,22 @@ var args = arguments[0] || {};
 
 Ti.API.info("ARGUMENTS PASSED TO GAME DETAIL: " + JSON.stringify(args));
 
-$.gameDetail.rightNavButton = Ti.UI.createButton({
-	backgroundImage: '/images/game_detail/share.png',
-	width: '43dp',
-	height: '29dp'
-});
+if (OS_IOS) {
+   var leftButton = Ti.UI.createButton({
+    title: ' Nearby',
+    backgroundImage: '/images/game_detail/backButton.png',
+    width: '58dp',
+    height: '29dp',
+    font: {fontSize: '12dp', fontWeight: 'bold'}
+  });
+  leftButton.addEventListener('click', function(){
+    Alloy.globals.tabgroup.activeTab.close($.gameDetail);
+  });
+  $.gameDetail.leftNavButton = leftButton;
+
+  $.gameDetail.rightNavButton = Ti.UI.createButton({
+    backgroundImage: '/images/game_detail/share.png',
+    width: '43dp',
+    height: '29dp'
+  });
+}
