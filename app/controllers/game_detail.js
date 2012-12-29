ff899c68420d7dd8966566aa48f33abc10476ab1
Bilal Quadri <bilalquadri92@gmail.com>
2012-12-29 18:54:54 -0500

    Nav buttons now part of view code

--- app/controllers/game_detail.js
+++ app/controllers/game_detail.js
@@ -2,22 +2,7 @@ var args = arguments[0] || {};
 
 Ti.API.info("ARGUMENTS PASSED TO GAME DETAIL: " + JSON.stringify(args));
 
-if (OS_IOS) {
-   var leftButton = Ti.UI.createButton({
-    title: ' Nearby',
-    backgroundImage: '/images/game_detail/backButton.png',
-    width: '58dp',
-    height: '29dp',
-    font: {fontSize: '12dp', fontWeight: 'bold'}
-  });
-  leftButton.addEventListener('click', function(){
-    Alloy.globals.tabgroup.activeTab.close($.gameDetail, {animated: true});
-  });
-  $.gameDetail.leftNavButton = leftButton;
 
-  $.gameDetail.rightNavButton = Ti.UI.createButton({
-    backgroundImage: '/images/game_detail/share.png',
-    width: '43dp',
-    height: '29dp'
-  });
+function popWindow(){
+  Alloy.globals.tabgroup.activeTab.close($.gameDetail, {animated: true});
 }
