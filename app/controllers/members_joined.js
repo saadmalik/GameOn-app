aa4a7fbd9f880c86411985eb990894ef9120ba9d
Saad Malik <saadfmalik@gmail.com>
2013-01-09 14:17:58 -0500

    Updates to members joined

--- app/controllers/members_joined.js
+++ app/controllers/members_joined.js
@@ -3,3 +3,11 @@ var args = arguments[0] || {};
 function popWindow(){
   Alloy.globals.tabgroup.activeTab.close($.membersJoined, {animated: true});
 }
+
+// var tableData = [];
+// for (var i = 0; i<5; i++){
+//   var row = Alloy.createController('members_row').getView();
+//   tableData.push(row);
+// }
+
+// $.table.setData(tableData);
